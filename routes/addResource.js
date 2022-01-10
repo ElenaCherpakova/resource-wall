923bf31be73addbda1538c6ae384eaf6fabc6764
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-10 15:15:07 +0000

    add addResource route

--- routes/addResource.js
+++ routes/addResource.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-app.get('/create', (res, req) => {
-  res.redirect('/addResource');
-}
+module.exports = (db) => {
+    router.post("/create", (req, res) => {
+    const newResource = {
+      title: req.body.title,
+      description: req.body.description,
+      url: req.body.url,
+      imageURL: req.body.imageURL,
+      category: req.body.category
+      }
+    console.log(newResource)
+    .then((result) => console.log(result))
+    // login(email, password).then((user) => {
+    //   if (!user) {
+    //     res.redirect("/login");
+    //     console.log(user.data[0]);
+    //   } else {
+    //     res.redirect("/");
+    //   }
+    // });
+  });
+  router.get("/create", (req, res) => {
+    res.render("addResource");
+  });
+
+  return router;
+};
