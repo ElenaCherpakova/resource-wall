4ac44b701eba08b86453bfd95919544de69cb86a
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-10 16:23:44 +0000

    post create route

--- routes/addResource.js
+++ routes/addResource.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+
 module.exports = (db) => {
     router.post("/create", (req, res) => {
-    const newResource = {
+    const resource = {
       title: req.body.title,
       description: req.body.description,
       url: req.body.url,
       imageURL: req.body.imageURL,
       category: req.body.category
       }
-    console.log(newResource)
-    .then((result) => console.log(result))
-    // login(email, password).then((user) => {
-    //   if (!user) {
-    //     res.redirect("/login");
-    //     console.log(user.data[0]);
-    //   } else {
-    //     res.redirect("/");
-    //   }
-    // });
+      const addResourceMain = function (resource) {
+        return db
+          .query(
+            `INSERT INTO resources(title, description, image, url, user_id)
+            VALUES ($1, $2, $3, $4, $5) RETURNING *`, [resource.title, resource.description, resource.imageURL, resource.url, 1]
+          )
+          .then ((result) => console.log(result.rows[0]))
+          .catch ((err) => {
+            console.log(err.message)
+          });
+      };
+      addResourceMain(resource)
+      .then((result) => {
+        console.log("result is: ", result);
+      })
   });
+
   router.get("/create", (req, res) => {
     res.render("addResource");
   });
