961ddc1704b91f4868ffb41dea4888713e53c596
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-10 19:24:14 +0000

    edited post.create route

--- routes/addResource.js
+++ routes/addResource.js
@@ -3,64 +3,49 @@ const router = express.Router();
 
 
 module.exports = (db) => {
-    router.post("/create", (req, res) => {
+  router.post("/create", (req, res) => {
     const resource = {
       title: req.body.title,
       description: req.body.description,
       url: req.body.url,
       imageURL: req.body.imageURL,
       category: req.body.category
-      }
-      const promises = [];
-      const promiseOne = db.query (
-        `INSERT INTO resources(title, description, image, url, user_id)
+    }
+    const promises = [];
+    const promiseOne = db.query(
+      `INSERT INTO resources(title, description, image, url, user_id)
          VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-        [resource.title, resource.description, resource.imageURL, resource.url, 1] /*user_id is hard coded*/
-      );
+      [resource.title, resource.description, resource.imageURL, resource.url, 1] /*user_id is hard coded*/
+    );
 
-      const promiseTwo = db.query(
-        `INSERT INTO categories (name)
+    const promiseTwo = db.query(
+      `INSERT INTO categories (name)
          VALUES ($1) RETURNING *`,
-        [resource.category]
+      [resource.category]
+    );
+
+    promises.push(promiseOne);
+    promises.push(promiseTwo);
+
+    Promise.all(promises)
+      .then(async (result) => {
+        const resource = result[0].rows[0];
+        const category = result[1].rows[0];
+        console.log(category.id, resource.id)
+        const promiseThree = await db.query(
+          `INSERT INTO categories_resources (category_id, resource_id)
+         VALUES ($1, $2) RETURNING *`,
+          [category.id, resource.id]
+
         );
 
-        // const promiseThree = db.query(
-        // `INSERT INTO categories_resources (category_id, resource_id)
-        //  VALUES ($1, $2) RETURNING *`,
-        // []
-        // );
+        console.log(promiseThree);
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err.message)
+      });
 
-        promises.push(promiseOne);
-        promises.push(promiseTwo);
-        //promises.push(promiseThree);
-
-        // console.log("promise one", promiseOne);
-        // console.log("promise two", promiseTwo);
-        Promise.all(promises)
-        .then ((result) => {
-          console.log(result);
-          //console.log(result.rows[0]);
-          res.redirect("/");
-        })
-            .catch ((err) => {
-              console.log(err.message)
-            });
-
-      // const addResourceMain = function (resource) {
-      //   return db
-      //     .query(
-      //       `INSERT INTO resources(title, description, image, url, user_id)
-      //       VALUES ($1, $2, $3, $4, $5) RETURNING *`, [resource.title, resource.description, resource.imageURL, resource.url, 1] /*user_id is hard coded*/
-      //     )
-      //     .then ((result) => console.log(result.rows[0]))
-      //     .catch ((err) => {
-      //       console.log(err.message)
-      //     });
-      // };
-      // addResourceMain(resource)
-      // .then((result) => {
-      //   console.log("result is: ", result);
-      // })
 
   });
 
