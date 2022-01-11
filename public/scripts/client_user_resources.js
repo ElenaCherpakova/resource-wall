a13b8dc7b2d333bba73d41a65bbebb4dc90222f4
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-11 23:23:58 +0000

    set up router post - user can edit the prfile, add ajax in client_user_resoirce file

--- public/scripts/client_user_resources.js
+++ public/scripts/client_user_resources.js
@@ -3,50 +3,65 @@
 
 
 $(document).ready(function () {
+  $("#update-profile").on("submit", (e) => {
+    e.preventDefault()
+    const firstName = $(this).find("input[name=firstName]").val()
+    const lastName = $(this).find("input[name=lastName]").val()
+    const password = $(this).find("input[name=password]").val()
+    const email = $(this).find("input[name=email]").val()
+    const data = { firstName, lastName, password, email }
+    $.ajax({
+      method: 'POST',
+      url: '/profile',
+      data: data,
+      success: (response) => {
+        console.log(response)
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+  })
+})
 
+// const createResource = function(resource, categories){
+//   const $resource = $('<div>').addClass('card-deck');
+//   const $divSecond = $('div').addClass('card').css({width: "18rem"});
+//   const $imgResource = $('img').addClass('card-img-top resource-img img-fluid').attr("src", resource.image); //did not att alt comment
+//   const $divThird = $('<div>').addClass('card-body');
+//   const $h5title = $('<h5>').addClass('card-title text-center').text(resource.title);
+//   const $divFouth = $('<div>');
+//   const $pDescription = $('<p>').addClass('card-text').text(resource.description);
+//   const $pLink = $('<p>').addClass('card-text').text(resource.url);
+//   const $pCategories= $('<p>').addClass('card-text tag').text(categories.name);
+//   const $divFifth = $('<div>').addClass('card-footer');
+//   const $icon1 = $('<i>').addClass('far fa-heart');
+//   const $icon2 = $('<i>').addClass('far fa-comment-dots');
+//   const $icon3 = $('<i>').addClass('far fa-star');
 
+//   //append element together
+//   $divFifth.append($icon1, $icon2, $icon3);
+//   $divFouth.append($pDescription, $pLink, $pCategories);
+//   $divThird.append($h5title, $divFouth);
+//   $divSecond.append($imgResource, $divThird, $divFouth);
+//   $resource.append($divSecond);
 
-});
+//   return $resource;
+// };
 
-const createResource = function(resource, categories){
-  const $resource = $('<div>').addClass('card-deck');
-  const $divSecond = $('div').addClass('card').css({width: "18rem"});
-  const $imgResource = $('img').addClass('card-img-top resource-img img-fluid').attr("src", resource.image); //did not att alt comment
-  const $divThird = $('<div>').addClass('card-body');
-  const $h5title = $('<h5>').addClass('card-title text-center').text(resource.title);
-  const $divFouth = $('<div>');
-  const $pDescription = $('<p>').addClass('card-text').text(resource.description);
-  const $pLink = $('<p>').addClass('card-text').text(resource.url);
-  const $pCategories= $('<p>').addClass('card-text tag').text(categories.name);
-  const $divFifth = $('<div>').addClass('card-footer');
-  const $icon1 = $('<i>').addClass('far fa-heart');
-  const $icon2 = $('<i>').addClass('far fa-comment-dots');
-  const $icon3 = $('<i>').addClass('far fa-star');
+// const renderUserResources = function (resources) {
+//   const $userResourcesContainer = $('#created-resources');
+//   $userResourcesContainer.empty();
+//   for (resource of resources){
+//     const $resource = createResource(resource, categories);
+//     $userResourcesContainer.prepend($resource);
+//   }
+// };
 
-  //append element together
-  $divFifth.append($icon1, $icon2, $icon3);
-  $divFouth.append($pDescription, $pLink, $pCategories);
-  $divThird.append($h5title, $divFouth);
-  $divSecond.append($imgResource, $divThird, $divFouth);
-  $resource.append($divSecond);
-
-  return $resource;
-};
-
-const renderUserResources = function (resources) {
-  const $userResourcesContainer = $('#created-resources');
-  $userResourcesContainer.empty();
-  for (resource of resources){
-    const $resource = createResource(resource, categories);
-    $userResourcesContainer.prepend($resource);
-  }
-};
-
-const renderLikedResources = function (resources) {
-  const $likedResourcesContainer = $('#liked-resources');
-  $likedResourcesContainer.empty();
-  for (resource of resources){
-    const $resource = createResource(resource, categories);
-    $likedResourcesContainer.prepend($resource);
-  }
-};
+// const renderLikedResources = function (resources) {
+//   const $likedResourcesContainer = $('#liked-resources');
+//   $likedResourcesContainer.empty();
+//   for (resource of resources){
+//     const $resource = createResource(resource, categories);
+//     $likedResourcesContainer.prepend($resource);
+//   }
--- routes/profile.js
+++ routes/profile.js
@@ -3,9 +3,69 @@ const router = express.Router();
 
 module.exports = (db) => {
 
-  router.get("/profile", (req, res) => {
-    res.render("user_profile");
-  });
+  // const selectExistedUser = db.query(
+  //   `SELECT * FROM users
+  //   WHERE users.id = $1;`,
+  //   [req.session.user_id])
 
+
+
+
+
+
+
+
+  router.get("/profile", (req, res) => {
+    if (!req.session.user_id) {
+      res.redirect("/login");
+    } else {
+      db.query(
+        `SELECT * FROM users
+        WHERE users.id = $1;`,
+        [req.session.user_id])
+
+        .then((result) => {
+          console.log("this IS RESULT", result.rows)
+          // const userResources = result[0].rows;
+          const selectedUser = result.rows[0]
+          console.log("this user=>", selectedUser)
+          const templateVars = { selectedUser };
+          res.render("user_profile", templateVars)
+        })
+        .catch((err) => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
+  })
+
+  router.post("/profile", (req, res) => {
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    }
+
+    console.log(req.body.firstName, req.body.lastName, req.body.email, req.body.password)
+
+    db.query(
+      `UPDATE users
+      SET first_name = $1,
+      last_name = $2,
+      email = $3,
+      password = $4
+      WHERE id = $5
+      RETURNING *;`, [user.firstName, user.lastName, user.email, user.password, req.session.user_id])
+
+      .then((result) => result.rows[0])
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message });
+        });
+    });
   return router;
 }
+
