3d535e1839d35193885e5000391b8aa9c973a16e
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-11 06:15:41 +0000

    tag input query completed

--- htmlresource.sql
+++ htmlresource.sql
@@ -7,3 +7,17 @@ LIMIT 1;
 JOIN categories_resources ON categories.id = category_id
 JOIN resources ON resources.id = resource_id
 WHERE resources.id = $1`
+
+SELECT resources.*, categories.name AS category_type
+FROM ((resources
+INNER JOIN categories_resources ON resources.id = resource_id)
+INNER JOIN categories ON categories.id = category_id)
+ORDER BY created_at DESC
+LIMIT 1;
+
+
+
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
