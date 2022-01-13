2a8e339b5dce6acda782cf9fc4a28096486dce85
TorontosFinest <abdialicanada20@gmail.com>
2022-01-13 01:55:50 +0000

    removed console.logs and updated header.ejs

--- public/scripts/rate.js
+++ public/scripts/rate.js
@@ -7,18 +7,14 @@ $(document).ready(function () {
       let id = starID.slice(-1);
       let resourceID = $(star).attr("resource_id");
       let title = $(star).attr("title");
-      console.log(star);
 
       $.ajax({
         method: "POST",
         url: "/rate",
         data: { starID: id, resourceID: resourceID },
         success: (response) => {
-          console.log("abc");
-          console.log("response is ", response.status);
           if (response.status === 201) {
             for (i = 1; i <= id; i++) {
-              console.log("this is ", i, title);
               document
                 .getElementById(`${title}-${i}`)
                 .classList.add("active-star");
--- routes/rate.js
+++ routes/rate.js
@@ -5,17 +5,13 @@ module.exports = (db) => {
   router.post("/rate", (req, res) => {
     const { starID, resourceID } = req.body;
     const user = req.session.user_id;
-    console.log("OUR INFO IS ", starID, resourceID);
-    console.log("USER IS ", user);
     if (user) {
-      console.log("i got it", user);
       return db
         .query(
           `SELECT * FROM ratings WHERE user_id = $1 AND resource_id = $2;`,
           [user, resourceID]
         )
         .then((result) => {
-          console.log("RESULT IS : ", result.rows);
           if (result.rows.length < 1) {
             return db
               .query(
