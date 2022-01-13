92d1e1989e4c1c9878a0aa055cab8305457c35d1
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-13 23:24:45 +0000

    fixed search.ejs to display like/search

--- htmlresource.sql
+++ htmlresource.sql
@@ -1,39 +1,11 @@
- <div class="commentSection">
-            <textarea
-              class="form-control"
-              placeholder="Leave your comments"
-              id="commentsText"
-              rows="4"
-              style="background: #fff"
-            ></textarea>
-            <label class="form-label" for="commentsText"></label>
-            <div class="float p-2">
-              <button type="button" id="post-comment" class="btn btn-primary btn-sm" resource_id = "<%=resources[resource].id %>">
-                Post comment
-              </button>
-              <button type="button" class="btn btn-outline-primary btn-sm">
-                Cancel
-              </button>
-            </div>
-          </div>
+SELECT * FROM resources WHERE title ILIKE $1
 
-
-
-                <div class="card-footer py-3 border-0" style="background-color: #f8f9fa;">
-                  <div class="d-flex flex-start w-100">
-
-                    <div class="form-outline w-100">
-                      <textarea
-                        class="form-control"
-                        id="textAreaExample"
-                        rows="4"
-                        style="background: #fff;"
-                      ></textarea>
-                      <label class="form-label" for="textAreaExample">Message</label>
-                    </div>
-                  </div>
-                  <div class="float-end mt-2 pt-1">
-                    <button type="button" class="btn btn-primary btn-sm">Post comment</button>
-                    <button type="button" class="btn btn-outline-primary btn-sm">Cancel</button>
-                  </div>
-                </div>
+SELECT resources.*, categories.name AS category_type, count(likes.*) AS like
+FROM resources
+RIGHT JOIN categories_resources ON resources.id = resource_id
+LEFT JOIN categories ON categories.id = category_id
+LEFT JOIN likes ON likes.resource_id = resources.id
+LEFT JOIN ratings ON ratings.resource_id = resources.id
+GROUP BY resources.id, categories.name
+HAVING title LIKE '%Light%'
+ORDER BY resources.created_at DESC
--- routes/search.js
+++ routes/search.js
@@ -5,11 +5,21 @@ module.exports = (db) => {
   router.post("/search", (req, res) => {
     const title = req.body.search;
     return db
-      .query("SELECT * FROM resources WHERE title ILIKE $1", [
-        "%" + title + "%",
-      ])
+      // .query("SELECT * FROM resources WHERE title ILIKE $1", [
+      //   "%" + title + "%",
+      // ])
+      .query (`SELECT resources.*, categories.name AS category_type, count(likes.*) AS like
+        FROM resources
+        RIGHT JOIN categories_resources ON resources.id = resource_id
+        LEFT JOIN categories ON categories.id = category_id
+        LEFT JOIN likes ON likes.resource_id = resources.id
+        LEFT JOIN ratings ON ratings.resource_id = resources.id
+        GROUP BY resources.id, categories.name
+        HAVING title ILIKE $1
+        ORDER BY resources.created_at DESC`, ["%" + title + "%"])
       .then((data) => {
         const resources = data.rows;
+        console.log("data.rows -->", data.rows)
         const templateVars = { resources };
         res.render("search", templateVars);
       })
